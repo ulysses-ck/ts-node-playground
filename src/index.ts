ca4bce4147a63a55883c79bb0e07e108467dd181
ulysses-ck <ulisesapaza.280@gmail.com>
2023-01-15 21:36:51 -0300

    Change: nodemon and ts-node by chokidar and only swc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const str: string = "Hello Typescript";
-console.log(str);
+import { IHuman } from "./types/Human";
+
+const John: IHuman = {
+	firstName: "John",
+	lastName: "Doe",
+	age: 32,
+	nationality: "French",
+	friends: ["Jane", "Maria", "Julia"]
+};
+
+console.log(John);
---
+++ src/types/Human.ts
@@ -0,0 +1,7 @@
+export interface IHuman {
+	firstName: string;
+	lastName: string;
+	age: number;
+	nationality: string;
+	friends: string[];
+}
